025d3eefdd316d0d5a9b677388b98197bbf4eee3
Jared Gilleland <ejgilleland@gmail.com>
2021-02-03 00:38:15 -0800

    Hid edit-profile, built new HTML for viewing a completed profile, defined a JS function that returns a DOM tree matching this new HTML #2

--- js/main.js
+++ js/main.js
@@ -16,3 +16,45 @@ $profileForm.addEventListener('submit', function (event) {
   $profileForm.reset();
   $avatarImg.setAttribute('src', './images/placeholder-image-square.jpg');
 });
+
+function profileRender() {
+  var $container = document.createElement('div');
+  $container.className = 'container';
+  $container.setAttribute('data-view', 'view-profile');
+  var $header = document.createElement('h1');
+  $header.textContent = data.profile.fullName;
+  var $row = document.createElement('div');
+  $row.className = 'row';
+  var $imgContainer = document.createElement('div');
+  $imgContainer.className = 'column-half image-container';
+  var $newAvatar = document.createElement('img');
+  $newAvatar.setAttribute('src', data.profile.avatarUrl);
+  var $textContainer = document.createElement('div');
+  $textContainer.className = 'column-half';
+  var $usernameHeader = document.createElement('h4');
+  var $personIconSpan = document.createElement('span');
+  $personIconSpan.className = 'icon';
+  var $personIcon = document.createElement('i');
+  $personIcon.className = 'fas fa-user';
+  var $locationHeader = document.createElement('h4');
+  var $locationIconSpan = document.createElement('span');
+  $locationIconSpan.className = 'icon';
+  var $locationIcon = document.createElement('i');
+  $locationIcon.className = 'fas fa-map=marker-alt';
+  var $bio = document.createElement('p');
+  $bio.textContent = data.profile.bio;
+
+  $container.append($header, $row);
+  $row.append($imgContainer, $textContainer);
+  $imgContainer.appendChild($newAvatar);
+  $textContainer.append($usernameHeader, $locationHeader, $bio);
+  $usernameHeader.appendChild($personIconSpan);
+  $personIconSpan.appendChild($personIcon);
+  $usernameHeader.textContent = data.profile.username;
+  $locationHeader.appendChild($locationIconSpan);
+  $locationIconSpan.appendChild($locationIcon);
+  $locationHeader.textContent = data.profile.location;
+  return $container;
+}
+
+document.querySelector('body').appendChild(profileRender());
