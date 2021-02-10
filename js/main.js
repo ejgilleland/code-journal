e0a7984fc230ce9c6fcc3c654c999461ac791cb8
Jared Gilleland <ejgilleland@gmail.com>
2021-02-09 20:51:51 -0800

    Listen for submit events on entry form, add entry values to an object, unshift() this object into data.entries, save to local storage, reset form inputs and img, change view to Entries

--- js/main.js
+++ js/main.js
@@ -27,6 +27,19 @@ $profileForm.addEventListener('submit', function (event) {
   $avatarImg.setAttribute('src', './images/placeholder-image-square.jpg');
 });
 
+$entryForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var entry = { photoUrl: null, title: null, notes: null };
+  for (const property in entry) {
+    entry[property] = $entryForm.elements[property].value;
+  }
+  data.entries.unshift(entry);
+  localStorage.setItem('data-profile', JSON.stringify(data));
+  $entryForm.reset();
+  $entryImg.setAttribute('src', './images/placeholder-image-square.jpg');
+  viewSwapper('entries');
+});
+
 document.addEventListener('click', function (event) {
   if (event.target.nodeName.toLowerCase() === 'a') {
     if ((event.target.dataset.view === 'view-profile' && data.profile.username) ||
